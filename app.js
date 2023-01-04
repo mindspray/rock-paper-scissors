1f6aa413c6990f32c13f632f0f6c1424d3ca5f87
mindspray <581796+mindspray@users.noreply.github.com>
2023-01-04 06:14:40 -0500

    Add structure, logic, and messaging of game

--- app.js
+++ app.js
@@ -1,90 +1,131 @@
-"use strict";
-
-function getPlayerSelection() {
-  let playerSelection = prompt("Rock, paper, scissors...", "").toLowerCase();
-
-  if (
-    playerSelection == "rock" ||
-    playerSelection == "paper" ||
-    playerSelection == "scissors"
-  ) {
-		return playerSelection;
-	}
-	console.log("Choose a valid option");
-	return getPlayerSelection();
-}
+'use strict';
 
 function getComputerChoice() {
   let rand = Math.floor(Math.random() * 3);
-  return rand === 0
-    ? "rock"
-    : rand === 1
-    ? "paper"
-    : "scissors";
+  return rand === 0 ? 'rock' : rand === 1 ? 'paper' : 'scissors';
 }
 
-function playRound(
-  playerSelection = getPlayerSelection(),
-  computerSelection = getComputerChoice()
-) {
-  console.log("Shoot!");
-  console.log(
-    `You chose: ${playerSelection}. Computer chose: ${computerSelection}`
-  );
+function playRound(playerChoice, computerChoice  = getComputerChoice()) {
+  playerChoice = playerChoice.toLowerCase();
+  dom.get("p1").textContent = "Rock, paper, scissors... shoot!";
+  dom.get("p2").textContent = `You chose ${playerChoice}. Computer chose: ${computerChoice}`;
+  
+  dom.get("body").appendChild(dom.get("div1"));
+  
+  let roundCount = state.get("rounds");
+  state.set("rounds", roundCount += 1);
 
-	// Potential refactor:
-	// Make choices = ["rock", "paper", "scissors"] array
+  let outcome;
 
-	// If choices[(indexof computerChoice) - 1] === choices[(indexof playerSelection)] { return "You win!"}
-	// e.g.
-	// choices[Array.indexOf("computerChoice") -1] === choices[Array.indexOf("playerSelection")] { return "You win!"}
-
-  // non-tie possibiliities
-  // rock v paper | paper
-  // rock v scissors | rock
-  // paper v scissors | scissors
-  // paper v rock | paper | dupe
-  // scissors v rock | rock | dupe
-  // scissors v paper | scissors | dupe
-  if (playerSelection === "rock" && computerSelection === "paper") {
-    return "You lose! Paper beats rock";
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return "You win! Paper beats rock";
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return "You lose! Rock beats scissors";
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return "You win! Rock beats scissors";
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return "You win! Scissors beats paper";
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return "You lose! Scissors beats paper";
+  if (playerChoice === 'rock' && computerChoice === 'paper') {
+    outcome = `Round ${roundCount}/5 lost! Paper beats rock`;
+  } else if (playerChoice === 'paper' && computerChoice === 'rock') {
+    outcome = `Round ${roundCount}/5 won! Paper beats rock`;
+  } else if (playerChoice === 'scissors' && computerChoice === 'rock') {
+    outcome = `Round ${roundCount}/5 lost! Rock beats scissors`;
+  } else if (playerChoice === 'rock' && computerChoice === 'scissors') {
+    outcome = `Round ${roundCount}/5 won! Rock beats scissors`;
+  } else if (playerChoice === 'scissors' && computerChoice === 'paper') {
+    outcome = `Round ${roundCount}/5 won! Scissors beats paper`;
+  } else if (playerChoice === 'paper' && computerChoice === 'scissors') {
+    outcome = `Round ${roundCount}/5 lost! Scissors beats paper`;
   } else {
-    return "It's a tie! Go again";
+    outcome = `Round ${roundCount}/5 is a tie!`;
   }
-  // }
+  
+  playGame(outcome);
 }
 
-function game() {
-  let rounds = [];
-  let score = 0;
-  for (let i = 0; i < 5; i++) {
-    rounds[i] = playRound();
-    if (rounds[i].includes("win")) {
-      score++;
-    } else if (rounds[i].includes("lose")) {
-      score--;
+function playGame(outcome, roundNum = state.get("rounds")) {
+  let score = state.get("score");
+  let running = !!roundNum;
+  dom.get("p3").textContent = `${outcome}`;
+  if (roundNum < 5) {
+    if (outcome.includes("win")){
+      state.set("score", score + 1);
+    } else if (outcome.includes("lose")){
+      state.set("score", score - 1);
+    }
+    dom.get("p4").textContent = "";
+  } else {
+    if (score > 0) {
+      dom.get("p4").textContent = "You win!!";
+    } else if (score < 0) {
+      dom.get("p4").textContent = "You lose.";
+    } else {
+      dom.get("p4").textContent = "It's a tie! Go again?";
+    }
+    score = 0;
+    state.set("rounds", 0);
+  }
+}
+
+function createElement(element, className = "") {
+  const el = document.createElement(element);
+  if (className) el.classList.add(className);
+  return el;
+}
+
+function init() {
+  const [buttonArray, elArray] = [[], []];
+  const rps = ["Rock", "Paper", "Scissors"];
+  const elems = ["button", "div", "p"];
+  const elemsCount = [3, 1, 4];
+  const body = document.querySelector("body");
+
+  // Create elements
+  for(let i = 0; i < elems.length; i++) {
+    for(let j = 0; j < elemsCount[i]; j++){
+      elArray.push(createElement(`${elems[i]}`, `${elems[i] + (j+1)}`));
+      if (elems[i] === "button") {
+        buttonArray.push(elArray[j]);
+        buttonArray[j].textContent = rps[j];
+        buttonArray[j].addEventListener("click", (e)=>playRound(e.target.textContent));
+      } else if (elems[i] === "div") {
+        body.appendChild(elArray.find(e => e.tagName == "DIV"));
+      } 
+      else if (elems[i] === "p") {
+        let divEl = elArray.find(e => e.tagName === "DIV");
+        divEl.appendChild(elArray[(i+1) + (j+1)]);
+      }
     }
   }
-  console.log(`The score is: ${score}`);
-  if (score > 0) {
-    console.log("You win the game!");
-  } else if (score < 0) {
-    console.log("You lost the game");
-  } else {
-    console.log("It was a tie! A rematch is in order");
+  
+  // Append 3 buttons to body
+  for (let i = 0; i < buttonArray.length; i++) {
+    body.appendChild(buttonArray[i]);
   }
 }
 
-// console.log(playRound(getPlayerSelection(), getComputerChoice()));
+init();
 
-game();
+const dom = {
+  _vars: {
+    body: document.querySelector("body"),
+    div1: document.querySelector(".div1"),
+    p1: document.querySelector(".p1"),
+    p2: document.querySelector(".p2"),
+    p3: document.querySelector(".p3"),
+    p4: document.querySelector(".p4"),
+  },
+
+  get(name) {
+    return this._vars[name];
+  },
+  set(name, value) {
+    this._vars[name] = value;
+  }
+}
+
+const state = {
+  _vars: {
+    rounds: 0,
+    score: 0,
+  },
+  get (name) {
+    return this._vars[name];
+  },
+  set (name, value) {
+    this._vars[name] = value;
+  }
+}
